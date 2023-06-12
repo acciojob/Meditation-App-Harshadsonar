480c1a7a20f8012bef9259fef0e8ce1a6fc34fc4
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-06-12 15:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,85 @@
-const meditationVideo = document.getElementById("meditation-video");
-const meditationAudio1 = document.getElementById("meditation-audio");
-const meditationAudio2 = document.getElementById("meditation-audio-2");
-const audio1Button = document.getElementById("audio-1");
-const audio2Button = document.getElementById("audio-2");
-const timeDisplay = document.querySelector(".time-display");
-const playButton = document.querySelector(".play");
-const timeSelectButtons = document.querySelectorAll("#time-select button");
+document.addEventListener("DOMContentLoaded", function() {
+  const app = document.getElementById("app");
+  const videoContainer = document.querySelector(".vid-container");
+  const video = document.getElementById("video");
+  const audio = document.getElementById("audio");
+  const soundButtons = document.querySelectorAll(".sound-picker button");
+  const timeButtons = document.querySelectorAll("#time-select button");
+  const timeDisplay = document.querySelector(".time-display");
+  const playButton = document.querySelector(".play");
 
-let meditationTimer = null;
-let remainingTime = 10 * 60;
+  let selectedSound = "sounds/beach.mp3";
+  let selectedVideo = "video/beach.mp4";
+  let meditationTime = 10 * 60; // Default time: 10 minutes
+  let intervalId;
 
-function setTime(minutes) {
-  remainingTime = minutes * 60;
-  updateTimeDisplay();
-}
+  // Video and audio setup
+  video.src = selectedVideo;
+  audio.src = selectedSound;
 
-function updateTimeDisplay() {
-  const minutes = Math.floor(remainingTime / 60);
-  const seconds = remainingTime % 60;
-  timeDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-}
+  // Sound selection
+  soundButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      soundButtons.forEach(function(btn) {
+        btn.classList.remove("selected");
+      });
+      this.classList.add("selected");
+      selectedSound = this.id === "sound1" ? "sounds/beach.mp3" : "sounds/rain.mp3";
+      audio.src = selectedSound;
+    });
+  });
 
-function startTimer() {
-  if (meditationTimer) {
-    return;
+  // Time selection
+  timeButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      timeButtons.forEach(function(btn) {
+        btn.classList.remove("selected");
+      });
+      this.classList.add("selected");
+      meditationTime = parseInt(this.id.replace("mins", "")) * 60;
+      updateTimeDisplay(meditationTime);
+    });
+  });
+
+  // Play/pause button
+  playButton.addEventListener("click", function() {
+    if (app.classList.contains("playing")) {
+      pauseMeditation();
+    } else {
+      startMeditation();
+    }
+  });
+
+  function startMeditation() {
+    app.classList.add("playing");
+    playButton.textContent = "Pause";
+    videoContainer.style.display = "block";
+    audio.play();
+    video.play();
+
+    intervalId = setInterval(function() {
+      meditationTime--;
+      updateTimeDisplay(meditationTime);
+
+      if (meditationTime <= 0) {
+        pauseMeditation();
+        updateTimeDisplay(0);
+      }
+    }, 1000);
   }
 
-  playButton.classList.add("playing");
-  meditationVideo.play();
-  meditationAudio1.play();
+  function pauseMeditation() {
+    app.classList.remove("playing");
+    playButton.textContent = "Play";
+    videoContainer.style.display = "none";
+    audio.pause();
+    video.pause();
+    clearInterval(intervalId);
+  }
 
-  meditationTimer = setInterval(() => {
-    remainingTime -= 1;
-    updateTimeDisplay();
+  function updateTimeDisplay(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    timeDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  }
+});
